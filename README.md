03eadd0c74d23e7bff70ad1d38e205b4bc93065e
AJ ONeal <coolaj86@gmail.com>
2022-10-04 02:39:01 -0600

    feat: standalone CORS proxy

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# cors-proxy.js
-A simple CORS proxy server with configurable allowed domains
+# cors-server.js
+
+A simple CORS proxy server with configurable allowed domains.
+
+See `example.env` for config options.
+
+## Install
+
+Install `node`:
+
+```sh
+curl https://webi.sh/node | sh
+source ~/.config/envman/PATH.env
+```
+
+Run `cors-server`
+
+```bash
+git clone https://github.com/dashhive/cors-server.js
+pushd ./cors-server.js/
+
+./bin/cors-server.js --prefix /api/cors/ --port 2677 --domains 'example.com, example.org'
+```
+
+## Usage
+
+### localhost https with Caddy
+
+`./Caddyfile`:
+
+```Caddyfile
+https://localhost:443 {
+    handle_path /api/* {
+        rewrite * /api{path}
+        reverse_proxy localhost:2677
+    }
+}
+```
+
+```sh
+curl https://webi.sh/caddy | sh
+source ~/.config/envman/PATH.env
+```
+
+```sh
+caddy run --config Caddyfile
+```
+
+### Test in browser
+
+```js
+await fetch("https://localhost/api/cors/example.com/", { mode: "cors" });
+```
+
+### Run as a Service
+
+```sh
+curl https://webi.sh/serviceman | sh
+source ~/.config/envman/PATH.env
+```
+
+```sh
+sudo env PATH="$PATH" \
+    serviceman add --system -- \
+    ./bin/cors-server.js --
+        --port 2677 \
+        --prefix /api/cors/ \
+        --domains example.com
+```
+
+```sh
+sudo env PATH="$PATH" \
+    serviceman add --system --cap-net-bind -- \
+    caddy run --config ./Caddyfile
+```
---
+++ bin/cors-server.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+"use strict";
+
+require("dotenv").config({ path: ".env" });
+require("dotenv").config({ path: ".env.secret" });
+
+let Http = require("http");
+
+let CorsProxy = require("../");
+
+async function main() {
+  let PORT = 2677; // 2(C) 6(O) 77(RS)
+  let portIndex = process.argv.indexOf("--port");
+  if (portIndex > 0) {
+    PORT = parseInt(process.argv[portIndex + 1], 10) || PORT;
+  } else {
+    PORT =
+      parseInt(process.env.NODE_PORT || process.env.PORT || "", 10) || PORT;
+  }
+
+  let prefix;
+  let prefixIndex = process.argv.indexOf("--prefix");
+  if (prefixIndex > 0) {
+    prefix = process.argv[prefixIndex + 1];
+  } else {
+    prefix = process.env.CORS_PROXY_PREFIX;
+  }
+
+  let allowedHostnamesStr;
+  let domainsIndex = process.argv.indexOf("--domains");
+  if (domainsIndex > 0) {
+    allowedHostnamesStr = process.argv[domainsIndex + 1];
+  } else {
+    allowedHostnamesStr = process.env.CORS_ALLOWED_DOMAINS;
+  }
+  let allowedHostnames = allowedHostnamesStr?.split(/[\s,]+/g) || [];
+  if (!allowedHostnames.length) {
+    console.error("please set CORS_ALLOWED_HOSTNAMES or --domains");
+    return;
+  }
+
+  let corsProxy = CorsProxy.create({
+    prefix,
+    allowedHostnames,
+  });
+
+  let server = Http.createServer(corsProxy.handler);
+  server.listen(PORT, function () {
+    console.info(`CORS Proxy listening on`, server.address());
+  });
+}
+
+main().catch(function (err) {
+  console.error("Fail:");
+  console.error(err.stack || err);
+  process.exit(1);
+});
